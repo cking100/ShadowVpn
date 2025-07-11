909a2358fc930635ef1ce4f38fe2ca10397b5040
chirag <c.taneja09@gmail.com>
2025-07-12 00:35:49 +0530

    Header file

--- vpn-frontend/src/components/Header.jsx
+++ vpn-frontend/src/components/Header.jsx
@@ -1,10 +1,36 @@
+import { useState } from "react";
 import "./Header.css";
 
 export default function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => setShowMenu(!showMenu);
+
   return (
-    <header className="header">
-      <h1>🛡️ Shadow Vpn</h1>
-      <button className="logout-btn">Logout</button>
-    </header>
+    <div className="header">
+      <div className="logo">
+        <h3>Shadow Vpn</h3>
+      </div>
+
+      <div className="header-right">
+        <button className="settings-icon">
+          <i className="fas fa-cog"></i>
+        </button>
+
+        <div className="profile">
+          <div className="avatar" onClick={toggleMenu}>
+            CT
+          </div>
+
+          {showMenu && (
+            <div className="profile-dropdown">
+              <p> View Profile</p>
+              <p> Edit Profile</p>
+              <p> Logout</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
